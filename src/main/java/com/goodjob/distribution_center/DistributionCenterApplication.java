2d0090c4274c26c3579574ef2e79f97be03fdaec
yiruli <wk_liyiru@163.com>
2021-02-28 10:18:26 +0800

    初始化springboot项目

---
+++ src/main/java/com/goodjob/distribution_center/DistributionCenterApplication.java
@@ -0,0 +1,13 @@
+package com.goodjob.distribution_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributionCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributionCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goodjob/distribution_center/DistributionCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodjob.distribution_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributionCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
